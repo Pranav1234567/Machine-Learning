be1d5e657f0da041660efe1259782f898938feee
Pranav1234567 <pnpiano@gmail.com>
2017-07-30 09:13:47 -0400

    Added a folder

---
+++ Neural-Networks-Learning/sigmoid.m
@@ -0,0 +1,17 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+g = zeros(size(z));
+
+if size(z) < 2,
+    g = 1/(1+e^(-1*z));
+else,
+    g = zeros(rows(z),columns(z));
+for j = 1:rows(z),
+    for k = 1:columns(z),
+        g(j,k) =1/(1+e^(-1*z(j,k)));
+    end;
+end;
+
+end;
